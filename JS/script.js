c69e4be67b0ed3f5eab3954c7e421726443eca7d
Ol-k121967 <oleg.koriakoff@yandex.ru>
2022-12-11 12:38:03 +0300

    add btn 1,2,3

--- JS/script.js
+++ JS/script.js
@@ -10,3 +10,12 @@ document.getElementById('btn_clr').addEventListener('click', function () {
     inputWindow.value = '';
 })
 
+document.getElementById('btn_1').addEventListener('click', function () {
+    inputWindow.value += '1';
+})
+document.getElementById('btn_2').addEventListener('click', function () {
+    inputWindow.value += '2';
+})
+document.getElementById('btn_3').addEventListener('click', function () {
+    inputWindow.value += '3';
+})
